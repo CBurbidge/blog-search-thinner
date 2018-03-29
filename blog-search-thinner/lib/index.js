142207fa427da0eef0f2c26fbd17e9b4edb4c305
Chester Burbidge <chester.burbidge@gmail.com>
2018-03-30 00:51:38 +0100

    tidied up

--- blog-search-thinner/lib/index.js
+++ blog-search-thinner/lib/index.js
@@ -12,8 +12,14 @@ var markdown = require('remark-parse');
 var html = require('remark-html');
 var remarkHtml = unified()
     .use(markdown)
-    .use(html)
+    .use(html);
 
+var frontmatterToPostData = function (post) {
+    var postFrontMatter = matter(post.text);
+    post.frontmatter = postFrontMatter.data;
+    post.text = postFrontMatter.content;
+    return post;
+}
 var markdownToHtml = function (post) {
     //var text = converter.makeHtml(post.text)
 
@@ -29,36 +35,22 @@ var markdownToHtml = function (post) {
 
     return Object.assign({}, post, { text });
 };
-
 var cleanHtmlOfPreTags = function (post) {
     var $ = cheerio.load(post.text);
     var minusPreTags = $("*").remove('pre');
     var text = $.html();
     return Object.assign({}, post, { text });
 }
-
 var removeHtml = function (post) {
     var text = htmlToText.fromString(post.text)
     return Object.assign({}, post, { text });
 }
-
-// todo make all post
-var removeCodeFromMarkdown = function (markdown) {
-    return removeHtml(cleanHtmlOfPreTags(markdownToHtml(removeHighlightCodeFromString(markdown))))
-}
-
-var removeCodeFromHtml = function (html) {
-    return removeHtml(cleanHtmlOfPreTags(removeHighlightCodeFromString(html)))
-}
-
 // http://web.archive.org/web/20080501010608/http://www.dcs.gla.ac.uk/idom/ir_resources/linguistic_utils/stop_words
 let stopWords = ["a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves"]
-
 function splitByWords(text) {
     var wordsArray = text.split(/\s+/);
     return wordsArray;
 }
-
 var filterOutDuplicates = function (element, index, array) {
     var elementIsFirstTimeSeen = index === array.indexOf(element)
     return elementIsFirstTimeSeen;
@@ -78,25 +70,14 @@ var filterApply = function (text, filtersArg) {
             return true;
         }).join(' ');
 }
-var filterWords = function (post) {
-    var filteredText = filterApply(post.text, [filterOutStopWords, filterOutDuplicates])
-    return Object.assign(post, { text: filteredText })
-}
-
 var removeNonAlpha = function (post) {
     var regex = /[^a-zA-Z0-9\-']/g
     var justAlpha = post.text.replace(regex, " ")
     return Object.assign(post, { text: justAlpha })
 }
-
 var toLowerCase = function (post) {
     return Object.assign(post, { text: post.text.toLowerCase() })
 }
-
-var cleanText = function (post) {
-    return filterWords(removeNonAlpha(post));
-}
-
 var removeHighlightCodeFromString = function (json) {
     var regexPattern = /\{%\s*(highlight|HIGHLIGHT)\s*[A-Za-z\-+#]*\s*%\}[^]*?\{%\s*endhighlight\s*%\}/gm;
     var current = json;
@@ -109,11 +90,8 @@ var removeHighlightCodeFromString = function (json) {
             matches = false;
         }
     }
-
     return current;
 }
-
-
 var getFrequentWordStripper = function (textArray, removePercentage) {
     function getAllWords(stringArray) {
         var wordsMap = Object.create(null);
@@ -210,14 +188,6 @@ var getFrequentWordStripper = function (textArray, removePercentage) {
         writeToFile, writeToFile
     }
 }
-
-var frontmatterToPostData = function (post) {
-    var postFrontMatter = matter(post.text);
-    post.frontmatter = postFrontMatter.data;
-    post.text = postFrontMatter.content;
-    return post;
-}
-
 var applyFuncs = function (testFuncs, logValues) {
     return data => {
         var returnVal = data;
@@ -233,8 +203,6 @@ var applyFuncs = function (testFuncs, logValues) {
         return returnVal;
     }
 }
-
-// todo parse frontmatter for markdown and return
 
 var configDefault = {
     parseFrontmatter: true,
@@ -327,8 +295,7 @@ module.exports = configArg => {
         filter: {
             outDuplicates: filterOutDuplicates,
             outStopWords: filterOutStopWords,
-            applyToSplitText: filterApply,
-            text: filterWords
+            applyToSplitText: filterApply
         }
     }
 }
